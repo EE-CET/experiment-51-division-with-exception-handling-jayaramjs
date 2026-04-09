a4b2a3b65626e859f77b9f44d9f974bf3b42e70f
Jayaram J S <89035356+jayaramjs@users.noreply.github.com>
2026-04-09 20:22:41 +0530

    Implement division handling with try-catch

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
 
 public class DivisionHandling {
-    
-        // TODO: Read two integers a and b
-        
-        // TODO: Enclose the division in a try block
-        
-        // TODO: Catch ArithmeticException
-        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // We use try-catch to monitor code that might throw an error
+        try {
+            // Read inputs
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            // This operation will throw an ArithmeticException if b is 0
+            int result = a / b;
+            
+            // This line only executes if the division was successful
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+            // This block executes ONLY if b was 0
+            System.out.println("Divide by zero error");
+        } finally {
+            // Good practice to close resources
+            sc.close();
+        }
+    }
 }
